7ec55438d67a1452276adbc155d7ccc900a8ef74
raisimoes <simooesangelo@gmail.com>
2023-11-17 16:58:42 -0300

    Commit questão 3

---
+++ MeuProjeto/dotNET - IP1/Questao 03/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Essa conversão pode ser feita através de um cast explícito.Porém, a parte fracionária
+será truncada e o valor será arredonda para o valor inteiro mais próximo em relação a zero.
+Se a magnitude da parte fracionária não puder ser representada como "int",
+ela será completamente truncada. */
+
+// Exemplo:
+using System;
+
+class MeuPrograma {
+    static void Main(){
+        // Variável do tipo double
+        double ValorDouble = 19.32;
+
+        // Convertendo o double para int usando um cast explícito
+        int ValorInt = (int)ValorDouble;
+
+        // Mostrando resultados
+
+        Console.WriteLine($"Valor double: {ValorDouble}");
+        Console.WriteLine($"Valor inteiro: {ValorInt}");
+    }
+}
