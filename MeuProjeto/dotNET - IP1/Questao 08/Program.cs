ca2cc00a570174b7037ce1066c26701fa8461e82
raisimoes <simooesangelo@gmail.com>
2023-11-17 17:24:18 -0300

    Commit atualizado questao 8

--- MeuProjeto/dotNET - IP1/Questao 08/Program.cs
+++ MeuProjeto/dotNET - IP1/Questao 08/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Variáveis
+        int num1 = 7;
+        int num2 = 3;
+        int num3 = 10;
+
+        // Verifica se num1 é maior do que num2
+        if (num1 > num2)
+        {
+            Console.WriteLine("num1 é maior do que num2.");
+        }
+        else
+        {
+            Console.WriteLine("num1 não é maior do que num2.");
+        }
+
+        // Verifica se num3 é igual a num1 + num2
+        if (num3 == num1 + num2)
+        {
+            Console.WriteLine("num3 é igual a num1 + num2.");
+        }
+        else
+        {
+            Console.WriteLine("num3 não é igual a num1 + num2.");
+        }
+    }
+}
