ffa16bbb3db05be71862d0fa39ff5c35e0c6381e
raisimoes <simooesangelo@gmail.com>
2023-11-17 17:22:06 -0300

    Commit atualizado questao 7

--- MeuProjeto/dotNET - IP1/Questao 07/Program.cs
+++ MeuProjeto/dotNET - IP1/Questao 07/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program{
+    static void Main(){
+        // Duas variáveis booleanas
+        bool condicao1 = true;
+        bool condicao2 = false
+
+        // Verifica se ambas as condições são verdadeiras
+        if (condicao1 && condicao2)
+        {
+            Console.WriteLine("Ambas as condições são verdadeiras.");
+        }
+        else
+        {
+            Console.WriteLine("Pelo menos uma das condições não é verdadeira.");
+        }
+    }
+}
