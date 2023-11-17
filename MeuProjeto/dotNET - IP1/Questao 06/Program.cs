78dfebcd872d4d1ed4d647a66b7bfe4547781c71
raisimoes <simooesangelo@gmail.com>
2023-11-17 17:17:41 -0300

    Commit questao 6

---
+++ MeuProjeto/dotNET - IP1/Questao 06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+class Program{
+    static void Main(){
+        // Duas strings
+        string str1 = "Hello";
+        string str2 = "World";
+
+        // Verificação se as duas strings são iguais
+        if (str1 == str2){
+            Console.WriteLine("As duas strings são iguais");
+        }
+        else{
+            Console.WriteLine("As duas strings são diferentes");
+        }
+    }
+}
