582570c4ad2dbc1ed285edddf04de33a4047d813
raisimoes <simooesangelo@gmail.com>
2023-11-17 17:12:28 -0300

    Commit questão 5

---
+++ MeuProjeto/dotNET - IP1/Questao 05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MeuPrograma{
+    static void Main (){
+        // Declaração e inicialização de variáveis
+        int a = 5;
+        int b = 8;
+
+        // Verificação se "a" > "b"
+        if (a > b){
+            Console.WriteLine("a é maior que b");
+        }
+        else{
+            Console.WriteLine("a não é maior que b");
+        }
+    }
+}
